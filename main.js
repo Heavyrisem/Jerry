7d0d4dcb1c1cb2e6f0409cb8811664d245366db5
heavyrisem <heavyrisem@gmail.com>
2021-08-12 04:29:06 +0900

    Introduce Jerry

---
+++ main.js
@@ -0,0 +1,34 @@
+
+let FrameLen = 1;
+let Quality = 'normal';
+let Images = {normal: new Array(), high: new Array()};
+
+function PreloadImage(e) {
+    [Quality, FrameLen] = e.target.value.split(" ");
+    // Image Preload
+    for (let i = 1; i <= FrameLen; i++) {
+        let img = new Image;
+        img.src = `./${Quality}/frame-${i}.png`;
+        Images[Quality][i-1] = img;
+    }
+}
+
+window.onload = () => {
+    const canvas = document.querySelector('canvas');
+    const context = canvas.getContext('2d');
+    const MaxScroll = (document.body.scrollHeight - document.body.clientHeight);
+    
+    function UpdateFrame() {
+        if (Images[Quality].length) {
+            if (window.scrollY == MaxScroll) window.scrollTo(0, 0);
+            let FrameNum = Math.min(FrameLen-1, Math.floor(window.scrollY / MaxScroll * FrameLen));
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(Images[Quality][FrameNum], 0, 0, Images[Quality][FrameNum].width, Images[Quality][FrameNum].height);
+        }
+        requestAnimationFrame(UpdateFrame);
+    }
+
+    requestAnimationFrame(UpdateFrame);
+
+}
